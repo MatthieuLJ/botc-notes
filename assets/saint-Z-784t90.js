5486eae33d3f28678021f73e05a94c075a068d05
Matthieu <matthieu.jeanson@gmail.com>
2024-09-08 23:47:58 -0700

    Updates

--- assets/saint-Z-784t90.js
+++
@@ -1,1 +0,0 @@
-import{C as t}from"./index-v7QKYiqY.js";const e="/botc-notes/assets/icon_saint-_85_P_cd.png",a={name:"Saint",icon:e,type:t.Outsider,ability:"If you die by execution, your team loses.",action:[]};export{a as default};
---
+++ assets/scarlet_woman-zc6nm3Bg.js
@@ -0,0 +1,1 @@
+import{C as e}from"./index-DPB1Cegl.js";const o="/botc-notes/assets/icon_scarletwoman-B3gcWorb.png",a={name:"Scarlet Woman",icon:o,type:e.Minion,ability:"If there are 5 or more players alive & the Demon dies, you become the Demon. (Travellers don't count)",action:[]};export{a as default};
